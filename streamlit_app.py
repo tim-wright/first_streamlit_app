4e2d9e6e9e7a37f3899c892c586a1c5d970acf5c
tim-wright <30731282+tim-wright@users.noreply.github.com>
2023-01-05 15:18:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,18 @@ if fruits_to_display.size > 0:
  
 
 streamlit.header('Fruityvice Food Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like info about?', 'Kiwi')
-streamlit.write('The user entered: ' + fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like info about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please specify a fruit to get informatino about it")
+  else:
+    streamlit.write('The user entered: ' + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 
 streamlit.stop()
 
