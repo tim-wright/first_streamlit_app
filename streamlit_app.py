bf418e3e006899769e45e61fd1cc4b77f32f4af6
tim-wright <30731282+tim-wright@users.noreply.github.com>
2023-01-05 15:29:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,12 +22,22 @@ fruits_to_display = my_fruit_list.loc[fruit_selection]
 if fruits_to_display.size > 0:
   streamlit.dataframe(fruits_to_display)
  
-
 def get_fruityvice_data(fruit):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+
+def get_fruit_load_list(my_cnx):
+  with my_cnx.cursor() as cur:
+    cur.execute("SELECT * from fruit_load_list")
+    return cur.fetchall()
+  
+def add_fruit_to_load_list(my_cnx, fruit_to_add):
+  with my_cnx.cursor() as cur:
+    cur.execute(f"INSERT INTO fruit_load_list values('{fruit_to_add}')")
+    return True
+  
 
 streamlit.header('Fruityvice Food Advice!')
 try:
@@ -41,19 +51,17 @@ try:
 except URLError as e:
   streamlit.error()
   
+    
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list(my_cnx)
+  streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
-
-fruit_to_add = streamlit.text_input('What fruit would you like info about?', 'jackfruit')
-my_cur = my_cnx.cursor()
-my_cur.execute(f"INSERT INTO fruit_load_list values('{fruit_to_add}')")
-streamlit.text("Thanks for adding: " + fruit_to_add)
+fruit_to_add = streamlit.text_input('What fruit would you like into add to Load table?')
+ 
+if streamlit.button('Add Fruit to Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list(my_cnx, fruit_to_add)
+  streamlit.text("Thanks for adding: " + fruit_to_add)
 
 
